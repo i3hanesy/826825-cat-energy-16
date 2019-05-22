0891e3fe7e211821a512b9cee733ee8972ab07ba
Andrey <i3hanesy@yandex.ru>
2019-05-23 01:46:19 +0500

    Исправление ошибок

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,13 @@ var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require(gulp-csso);
 
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
+var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -21,6 +23,8 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("stylemin.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
@@ -46,7 +50,7 @@ gulp.task("sprite" , function() {
   return gulp.src("source/img/icon-*.svg")
     .pipe(svgstore({inLineSvg: true}))
     .pipe(rename("sprite-icon.svg"))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("server", function () {
@@ -62,4 +66,23 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
+gulp.task("copy", function () {
+  return gulp.src([
+  "source/fonts/**/*.{woff,woff2}",
+  "source/img/**",
+  "source/js/**",
+  "source/*.ico"],
+  {base: "source"
+})
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+
 gulp.task("start", gulp.series("css", "server"));
+
+gulp.task("build" , gulp.series("clean" , "copy" , "css" , "sprite"));
+gulp.task("start" , gulp.series("build" , "server"));
